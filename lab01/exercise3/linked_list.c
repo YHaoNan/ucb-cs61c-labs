6657eee2d205f337a3add3054b82f3b890cce4cd
Yudoge <1355265122@qq.com>
2023-06-06 10:27:06 +0800

    Lab01 done

--- lab01/exercise3/linked_list.c
+++ lab01/exercise3/linked_list.c
@@ -55,7 +55,7 @@ void print_list(struct Node *head) {
 
 /* Iteratively reverses a linked list whose first node is HEAD */
 void reverse_list(struct Node **head) {
-    if (head == NULL) {
+    if (head == NULL || *head == NULL) {
         return;
     }
     struct Node *curr = *head;
@@ -77,6 +77,10 @@ void add_to_back(Node **head, int data) {
         return;
     }
     Node *new_node = create_node(data);
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    }
     Node *prev;
     for (Node *curr = *head; curr != NULL; curr = curr->next) {
         prev = curr;
--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,14 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    if (head==NULL) return 0;
+    node *fp = head, *sp = head;
+
+    do {
+        if (fp->next == NULL) return 0;
+        fp = fp->next->next;
+        sp = sp->next;
+    } while (fp != sp && fp != NULL && sp != NULL);
+
+    return fp == sp;
 }
